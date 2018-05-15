1900974cc455936dde91da522f5d95defa0cb191
afifkhaidir-tkpd <khaidir.afif@tokopedia.com>
2018-05-15 19:04:28 +0700

    Add optimization, separate dev and build config

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,14 @@
 const path = require('path')
+const devMode = process.env.NODE_ENV !== 'production'
+
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin')
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
 
 module.exports = {
+  mode: devMode ? 'development' : 'production',
   entry: './src/js/info.js',
+  devtool: 'source-map',
   output: {
     filename: 'info.min.js',
     path: path.resolve(__dirname, 'dist')
@@ -10,8 +17,52 @@ module.exports = {
     rules: [
       {
         test: /\.scss$/,
-        use: ['style-loader', 'css-loader', 'sass-loader']
+        use: [
+          devMode ? 'style-loader' : MiniCssExtractPlugin.loader,
+          { 
+            loader: 'css-loader', 
+            options: {
+              sourceMap: devMode 
+            }
+          },
+          { 
+            loader: 'sass-loader', 
+            options: {
+              sourceMap: devMode 
+            }
+          }
+        ]
+      },
+      {
+        test: /\.(png|svg|jpe?g|gif)$/,
+        use: ['file-loader']
       }
     ]
   },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: devMode ? "[name].css" : "[name].[hash].css",
+      chunkFilename: devMode ? "[id].css" : "[name].[hash].css"
+    })
+  ],
+  optimization: {
+    minimizer: [
+      new UglifyJsPlugin({
+        cache: true,
+        parallel: true,
+        sourceMap: devMode
+      }),
+      new OptimizeCssAssetsPlugin({})
+    ],
+    splitChunks: {
+      cacheGroups: {
+        styles: {
+          name: 'styles',
+          test: /\.css$/,
+          chunks: 'all',
+          enforce: true
+        }
+      }
+    }
+  }
 }
